40be446b6a6ce3324c8ff631d9795c2311ee57c3
Brunadm <61566317+Brunadm@users.noreply.github.com>
2020-06-03 02:30:49 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+$("#exampleinputTelefone").mask('(00) 00000-0000');
+
+$("#exampleinputCPF").mask('000.000.000-00');
+
+$("#exampleinputNascimento").mask('00/00/0000');
+
+$("#exampleinputCEP").mask('00000-000');
+
+$("#formulario").validade();
